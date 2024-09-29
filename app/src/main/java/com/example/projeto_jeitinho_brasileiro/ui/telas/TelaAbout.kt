e26bf64589ea61795a75d3068f8891818d0d4d22
Geraldo Neto <netogeraldo209@gmail.com>
2024-09-28 21:46:19 -0300

    adicionar alertas de confirmaçao saida

--- app/src/main/java/com/example/projeto_jeitinho_brasileiro/ui/telas/TelaAbout.kt
+++ app/src/main/java/com/example/projeto_jeitinho_brasileiro/ui/telas/TelaAbout.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +34,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.projeto_jeitinho_brasileiro.R
 import com.example.projeto_jeitinho_brasileiro.repositorio.user.Usuario
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.TextButton
+
 
 @Composable
 fun TelaAbout(
@@ -41,6 +48,8 @@ fun TelaAbout(
     onLogoffClick: () -> Unit,
     onBackClick: () -> Unit
 ) {
+    var showLogoffConfirmation by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -112,9 +121,30 @@ fun TelaAbout(
 
             Spacer(modifier = Modifier.size(24.dp))
 
-            Button(onClick = onLogoffClick) {
-                Text(text = "Sair", fontSize = 18.sp)
+            Button(onClick = { showLogoffConfirmation = true }) {
+                Text(text = "Desconectar", fontSize = 18.sp)
             }
         }
     }
+    if (showLogoffConfirmation) {
+        AlertDialog(
+            onDismissRequest = { showLogoffConfirmation = false },
+            title = { Text(text = "Deseja desconectar?") },
+            text = { Text("Tem certeza que deseja desconectar?") },
+            confirmButton = {
+                TextButton(onClick = {
+                    showLogoffConfirmation = false
+                    onLogoffClick()
+                }) {
+                    Text("Sim")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showLogoffConfirmation = false }) {
+                    Text("Não")
+                }
+            }
+        )
+    }
+
 }

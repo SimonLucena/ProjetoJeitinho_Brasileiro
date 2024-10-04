fc63c0b8dd30dc651a7b5c242bcd539c75fbb293
Geraldo Neto <netogeraldo209@gmail.com>
2024-10-04 12:51:34 -0300

    RECEBA A TOPBAR

--- app/src/main/java/com/example/projeto_jeitinho_brasileiro/ui/telas/TelaCart.kt
+++ app/src/main/java/com/example/projeto_jeitinho_brasileiro/ui/telas/TelaCart.kt
@@ -1,4 +1,4 @@
-package com.example.projeto_jeitinho_brasileiro.ui.telas
+package com.example.projeto_jeitinho_brasileiro.ui.telas;
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background

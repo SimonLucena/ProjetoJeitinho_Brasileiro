6a692fe6eb33e8ca1e373376e8e83950d862cc14
SimonLucena <lucena.simon@academico.ifpb.edu.br>
2024-10-03 22:39:33 -0300

    Ajustes

--- app/src/main/java/com/example/projeto_jeitinho_brasileiro/repositorio/receita/Receita.kt
+++ app/src/main/java/com/example/projeto_jeitinho_brasileiro/repositorio/receita/Receita.kt
@@ -6,37 +6,6 @@ data class Receita(
     val indice: Int = 0,
     var arquivo: String = "",
     var descricao: String = "",
-    var nome: String = ""
-) {
-
-}
-
-class Receitas {
-    private val repositorio = mutableListOf<Repositorio>()
-
-    fun addReceita(receita: Repositorio) {
-        repositorio.add(receita)
-    }
-
-    // Método público para obter o tamanho da lista repositorio
-    fun getTamanhoRepositorio(): Int {
-        return (repositorio.size-1)
-    }
-
-    fun getListaRepositorio(): List<Repositorio>{
-        return repositorio
-    }
-
-    fun getArquivo(index: Int): String?{
-        val imagem = repositorio.find { it.indice == index }
-        return imagem?.arquivo
-    }
-    fun getTitulo(index: Int): String?{
-        val imagem = repositorio.find { it.indice == index }
-        return imagem?.titulo
-    }
-    fun getDescricao(index: Int): String?{
-        val imagem = repositorio.find { it.indice == index }
-        return imagem?.descricao
-    }
-}
+    var nome: String = "",
+    var preco: Float
+) {}
